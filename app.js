4d0a4db855c5ae60b5dd64188d62b113b5ba1d82
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-03-18 17:28:25 +0100

    ADD: cors

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from "express"
+import cors from 'cors'
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
 import movieRouter from "./routers/movieRouter.js"
 import imagePathMiddleware from "./middlewares/imagePath.js"
+
+app.use(cors({
+    origin: process.env.FRONTEND_APP
+}))
 
 app.use(express.static('public'))
 
